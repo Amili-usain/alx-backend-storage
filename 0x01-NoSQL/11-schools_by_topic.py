643bddd6593a7707e87b3b4787bdfdcc1f04b801
Amili-usain <amilihussein073@gmail.com>
2023-05-09 05:41:15 +0000

    Where can I learn Python?

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,6 +5,7 @@ Returns the list of schools having a specific topic.
 
 import pymongo
 
+
 def schools_by_topic(mongo_collection, topic):
     """Returns the list of schools in the collection that have the specified
 topic."""
